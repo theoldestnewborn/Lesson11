b3c0a3337adc0d24430cd807c16236c78befb698
Aleksei <iamviga@yandex.ru>
2022-03-30 01:26:05 +0300

    Initial Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,26 +1,33 @@
 package com.company;
 
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String doc = sc.next();
+        String doc = "abc-1111-cba-2222-1a2b";
+
         if (doc.contains("abc") != true) {
             try {
-                throw new IOException();
-            } catch (IOException e) {
-                System.out.println("no abc found");
+                throw new MyException();
+            } catch (MyException e) {
+                System.out.println("No abc found");;
             }
         }
 
         if (doc.startsWith("555") != true) {
             try {
-                throw new IOException();
-            } catch (IOException e) {
+                throw new MyException();
+            } catch (MyException e) {
                 System.out.println("Doesnt start with 555");;
+            }
+        }
+
+        if (doc.endsWith("1a2b") != true) {
+            try {
+                throw new MyException();
+            } catch (MyException e) {
+                System.out.println("Doesnt end with 1a2b");;
             }
         }
     }
